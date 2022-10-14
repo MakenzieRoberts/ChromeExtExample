b000e93f3223fb7ac6021a4c96a40c5b87616e55
MakenzieRoberts <makenzie.roberts@keyin.com>
2022-10-14 12:20:08 -0230

    Added background page logging variable
    To view console logs from options.js on the background page of the extension, I needed to use chrome.extension.getBackgroundPage().console.log.

--- js/options.js
+++ js/options.js
@@ -1,29 +1,37 @@
+const log = chrome.extension.getBackgroundPage().console.log;
+
 function save() {
-    var sitesToSave = document.getElementById('sites').value;
-    var displayPopup = document.getElementById('popup').checked;
-    chrome.storage.sync.set({
-        sites: sitesToSave,
-		popup: displayPopup
-    }, displaySavedMessage);
+	var sitesToSave = document.getElementById("sites").value;
+	var displayPopup = document.getElementById("popup").checked;
+	chrome.storage.sync.set(
+		{
+			sites: sitesToSave,
+			popup: displayPopup,
+		},
+		displaySavedMessage
+	);
 }
 
 function load() {
-    chrome.storage.sync.get({
-        sites: 'supersecretsite.com',
-		popup: false
-    }, function(items) {
-        document.getElementById('sites').value = items.sites;
-        document.getElementById('popup').checked = items.popup;
-    });
+	chrome.storage.sync.get(
+		{
+			sites: "https://<your-prefix>/",
+			popup: false,
+		},
+		function (items) {
+			document.getElementById("sites").value = items.sites;
+			document.getElementById("popup").checked = items.popup;
+		}
+	);
 }
 
 function displaySavedMessage() {
-    var status = document.getElementById('status');
-    status.textContent = 'Sites saved.';
-    setTimeout(function() {
-        status.textContent = '';
-    }, 2000);
+	var status = document.getElementById("status");
+	status.textContent = "Sites saved.";
+	setTimeout(function () {
+		status.textContent = "";
+	}, 2000);
 }
 
-document.addEventListener('DOMContentLoaded', load);
-document.getElementById('save').addEventListener('click', save);
+document.addEventListener("DOMContentLoaded", load);
+document.getElementById("save").addEventListener("click", save);
